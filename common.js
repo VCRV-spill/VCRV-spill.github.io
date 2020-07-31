39638e7ab73c46232472ab1feb1ef2a647abc3cc
vcrv <no@no.no>
2020-08-01 04:04:17 +0800

    6

--- common.js
+++ common.js
@@ -1,2 +1,2 @@
-const Q=(el,func)=>func? document.querySelectorAll(el).forEach(func):document.querySelector(el);
-const cookie = key => document.cookie.split(/;\s?/).map( o => o.split('=') ).find( ([k,v]) => k == key );
+const Q = (el, func) => func ? document.querySelectorAll(el).forEach(func) : document.querySelector(el);
+const cookie = key => document.cookie.split(/;\s?/).map(o => o.split('=')).reduce( (obj, [k,v]) => ({...obj, [k]:v}) , {});
